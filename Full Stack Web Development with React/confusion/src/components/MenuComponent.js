76ef29f7ee53a3627bd229ab6ba359f9abd85683
Naren <naren.p.2018.cse@ritchennai.edu.in>
2020-12-06 20:14:46 +0530

    React Week 1 Assignment

---
+++ Full Stack Web Development with React/confusion/src/components/MenuComponent.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import DishDetailComponent from "./DishDetailComponent";
+
+class Menu extends Component {
+    
+        constructor(props) {
+            super(props);
+
+            this.state = {
+               selectedDish: null
+              }
+                
+        }
+
+        onDishSelect(dish) {
+            this.setState({ selectedDish: dish});
+        }
+
+        renderDish(dish) {
+            if (dish != null)
+            return <DishDetailComponent dish ={dish} />;
+            else return <div />
+        }
+
+        render() {
+
+            const menu = this.props.dishes.map((dish)=> {
+
+                return(
+                    <div key={dish.id} className="col-12 col-md-5 m-1">
+                        < Card onClick={()=> this.onDishSelect(dish)}>
+                                <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                            <CardImgOverlay>
+                                <CardTitle>{dish.name}</CardTitle>
+                                
+                            </CardImgOverlay>
+                            </Card>
+                        </div>
+
+
+                );
+            });
+
+            return (
+                <div className="container">
+                    <div className="row">
+                           {menu}
+                    </div>
+                    <div className ="row">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>
+                </div>
+            );
+        }
+    
+}
+
+export default Menu;
