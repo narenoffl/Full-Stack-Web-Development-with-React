e8bc1eec062c618d827c5c003bd4aa4b0c38b8ad
Naren <naren.p.2018.cse@ritchennai.edu.in>
2020-12-26 21:12:43 +0530

    Header and Footer Components

--- Full Stack Web Development with React/confusion/src/components/MainComponent.js
+++ Full Stack Web Development with React/confusion/src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetailComponent from "./DishDetailComponent";
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
   constructor(props) {
@@ -21,15 +22,12 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes= {this.state.dishes}
          onClick={(dishId)=> this.onDishSelect(dishId)} />
         <DishDetailComponent 
            dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish ) [0] } />
+        <Footer />      
       </div>
     );
   }
--- Full Stack Web Development with React/confusion/src/index.js
+++ Full Stack Web Development with React/confusion/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
