3e3ceaefbe194a25b00fb290ed6e54cccea3ad33
Naren <naren.p.2018.cse@ritchennai.edu.in>
2020-12-29 23:32:24 +0530

    Assignment 2 React Router and Single Page Applications

--- Full Stack Web Development with React/confusion/src/components/MainComponent.js
+++ Full Stack Web Development with React/confusion/src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -51,6 +52,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> } />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />      
