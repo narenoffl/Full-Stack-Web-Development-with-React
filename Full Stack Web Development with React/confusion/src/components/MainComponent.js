efc93b8be45434b38ec0307c9cdb85cc093e48f3
Naren <naren.p.2018.cse@ritchennai.edu.in>
2020-12-29 18:14:39 +0530

    Single Page Applications Part 1

--- Full Stack Web Development with React/confusion/src/components/MainComponent.js
+++ Full Stack Web Development with React/confusion/src/components/MainComponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from "./ContactComponent";
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetailComponent from "./DishDetailComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -13,6 +17,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
       
     };
   }
@@ -22,7 +29,10 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]} 
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
     
@@ -32,6 +42,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route ecact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />      
